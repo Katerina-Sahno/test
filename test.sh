7a832002e3ee4b04a6e3265153d7ba497ccf6885
Katerina-Sahno <katyusha.sahno@gmail.com>
2019-10-29 11:04:41 +0300

    the task for Server/System Administrator

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dir=/root/backup/
+namefile=*.gz
+
+countline=$(find $dir -type f -name $namefile -mtime +$1 | wc -l)
+echo $countline
+
+if (($countline>=$2));then
+        deleteFile=$(find $dir -type f -name $namefile -mtime +$1 -exec stat -c "%y %n" {} \; | sort -n | head -1 | cut -d' ' -f4)
+        echo "rm $deleteFile"
+        rm $deleteFile
+fi
+
+exit 0
