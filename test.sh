eeea1ed958f251429422d35f379acda867e17841
Katerina-Sahno <47814646+Katerina-Sahno@users.noreply.github.com>
2019-10-29 12:08:40 +0300

    Test for Server/System Administrator
    Test for Server/System Administrator

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ dir=/root/backup/
 namefile=*.gz
 
 countline=$(find $dir -type f -name $namefile -mtime +$1 | wc -l)
+
 if (($countline>=$2));then
         deleteFile=$(find $dir -type f -name $namefile -mtime +$1 -exec stat -c "%y %n" {} \; | sort -n | head -1 | cut -d' ' -f4)
         echo "rm $deleteFile"
