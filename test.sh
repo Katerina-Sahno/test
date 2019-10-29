4dcee7e005f4e2b633f03c6620ca7495ed1f875f
Katerina-Sahno <47814646+Katerina-Sahno@users.noreply.github.com>
2019-10-29 12:07:26 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,8 +4,6 @@ dir=/root/backup/
 namefile=*.gz
 
 countline=$(find $dir -type f -name $namefile -mtime +$1 | wc -l)
-echo $countline
-
 if (($countline>=$2));then
         deleteFile=$(find $dir -type f -name $namefile -mtime +$1 -exec stat -c "%y %n" {} \; | sort -n | head -1 | cut -d' ' -f4)
         echo "rm $deleteFile"
